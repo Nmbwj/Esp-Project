b11748e93c32ec44be91e39959aff1393c952909
Nmbwj <84346757+Nmbwj@users.noreply.github.com>
2024-09-09 15:58:38 +0300

    Simple database
    A record of random file access

---
+++ randomFileAccess/personal.h
@@ -0,0 +1,39 @@
+//************************ personal.h **************************
+
+#ifndef PERSONAL
+#define PERSONAL
+
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+class Personal {
+public:
+	Personal();
+	Personal(char*, char*, char*, int, long);
+	void writeToFile(fstream&) const;
+	void readFromFile(fstream&);
+	void readKey();
+	int size() const {
+		return 9 + nameLen + cityLen + sizeof(year) + sizeof(salary);
+	}
+	bool operator==(const Personal & pr) const {
+		return strncmp(pr.SSN, SNN, 9) == 0;
+	}
+protected:
+	const int nameLen, cityLen;
+	char SSN[10], *name, *city;
+	int year;
+	long salary;
+	ostream& writeLegibly(ostream&);
+	friend ostream& operator<<(ostream& out, Personal& pr){
+		return pr.writeLegibly(out);
+	}
+	istream& readFromConsole(istream&);
+	friend istream& operator>>(istream& in, Personal& pr){
+		return pr.readFromConsole(in);
+	}
+};
+#endif
+
